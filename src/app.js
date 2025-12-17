600b01f362285b9b8602c4085ee77aeace12eb94
Rohab_Amir <rohabamir2@gmail.com>
2025-12-17 12:58:56 +0500

    Route to handle user sign-up and added dynamic data in signup api

--- src/app.js
+++ src/app.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const connectDB = require('./config/database');
 const app = express();
+const User = require('./models/user');
+
+app.use(express.json()); // Middleware to parse JSON bodies
+
+// Route to handle user sign-up
+app.post('/signUp', async (req, res) => {
+
+    //Creating a new instance of user model
+    const user = new User(req.body);
+
+    try {
+        await user.save();
+        res.send("User created successfully");
+    } catch (error) {
+        res.status(400).send("Error creating user: " + error.message);
+    }
+});
+
+
 
 connectDB().then(() => {
     console.log('Database connected successfully');
