c86b91502bbf277975b8731cf016fa453fbdbd8d
Rohab_Amir <rohabamir2@gmail.com>
2025-12-24 12:40:28 +0500

    Feed Api Formation

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,7 @@ const userRouter = express.Router();
 
 const { userAuth } = require('../middlewares/auth');
 const ConnectionRequest = require('../models/connectionRequest');
+const User = require('../models/user');
 
 // Get all the pending connection requests for the authenticated user
 userRouter.get('/user/requests/recieved', userAuth, async (req, res) => {
@@ -52,5 +53,46 @@ userRouter.get('/user/connections', userAuth, async (req, res) => {
         res.status(400).send("ERROR : " + error.message);
     }
 });
+
+// Feed Api -> Gets you the profiles of the other users on the application
+userRouter.get('/user/feed', userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        // Get all connection requests ( send + recieved ) involving the logged-in user
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                { fromUserId: loggedInUser._id },
+                { toUserId: loggedInUser._id }
+            ]
+        }).select('fromUserId toUserId status');
+
+        //1. Extract user IDs that are already connected or have pending requests
+        const hiderUserIds = new Set();
+        connectionRequests.forEach(request => {
+            hiderUserIds.add(request.fromUserId.toString());
+            hiderUserIds.add(request.toUserId.toString());
+        });
+
+        //2. Fetch users excluding those in hiderUserIds and also exculding self
+        const users = await User.find({
+            $and: [
+                { _id: { $nin: Array.from(hiderUserIds) } },
+                { _id: { $ne: loggedInUser._id } }
+            ]
+        }).select('firstName lastName photoUrl gender about skills');   
+
+        res.send(users);
+
+
+
+    } catch (error) {
+        res.status(400).send("ERROR : " + error.message);
+    }
+});
+
+
+
+
 
 module.exports = userRouter;    
